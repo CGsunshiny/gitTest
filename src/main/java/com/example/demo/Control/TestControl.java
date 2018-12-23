37a7d479cddf9398794d44452f1da251c7913013
CGsunshiny <1206640707@qq.com>
2018-12-23 22:21:31 +0800

    第一次提交

---
+++ src/main/java/com/example/demo/Control/TestControl.java
@@ -0,0 +1,17 @@
+package com.example.demo.Control;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@ResponseBody
+@RequestMapping("/test")
+public class TestControl {
+
+    @RequestMapping("/use_git")
+    public String gitTest(){
+
+        return "你好这是我的第一次使用git在idea上面2232";
+
+    }
+
+}
